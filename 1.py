d7f50cda50343cb50d8117010f72b36424a7bc88
stringcoretech <150322088+stringcoretech@users.noreply.github.com>
2024-04-22 22:49:33 +0530

    list program 1

---
+++ 1.py
@@ -0,0 +1,33 @@
+fruits = ['apple','banana','orange','mango']
+
+print(fruits[0])
+print(fruits[1])
+print(fruits[2])
+print(fruits[3])
+print(fruits)
+
+fruits.append('grapes')
+print(fruits)
+
+fruits.remove('orange')
+print(fruits)
+print(len(fruits))
+
+print('apple' in fruits)
+print('kiwi' in fruits)
+fruits.sort()
+print(fruits)
+
+fruits.reverse()
+print(fruits)
+
+print(fruits[1:3])
+
+fruits_copy = fruits.copy()
+print(fruits_copy)
+
+
+
+
+
+
